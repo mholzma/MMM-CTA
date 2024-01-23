9f8a22fdfc7a2f4e217f2283bf1bb8c3c8a24c5c
Jordan Welch <jordanhwelch@gmail.com>
2024-01-22 20:40:44 -0600

    unwrap test

--- __tests__/templates/MMM-CTA.njk.spec.js
+++ __tests__/templates/MMM-CTA.njk.spec.js
@@ -2,13 +2,11 @@ nunjucks = require('../../__mocks__/nunjucks');
 
 translate = (str) => str;
 
-describe('MMM-CTA.njk', () => {
-  describe('loading', () => {
-    it('shows loading', () => {
-      const payload = { loading: true };
-      const template = nunjucks.render('MMM-CTA.njk', payload);
+describe('loading', () => {
+  it('shows loading', () => {
+    const payload = { loading: true };
+    const template = nunjucks.render('MMM-CTA.njk', payload);
 
-      expect(template).toContain('LOADING');
-    });
+    expect(template).toContain('LOADING');
   });
 });
